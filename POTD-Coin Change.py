17cb6e34b4d47b090d375c16a49608cdb70d4081
Amar <AmarnathRao36@gmail.com>
2023-08-11 14:24:19 +0530

    11aug

--- POTD-Coin Change.py
+++ POTD-Coin Change.py
@@ -2,28 +2,19 @@
 
 class Solution:
     def count(self, coins, N, Sum):
-        def solve(i,curSum,coins,Sum,dp):
-            if(curSum==Sum):
-                dp[i][curSum]=1
-                return dp[i][curSum]
-                
-            if(i==len(coins)):
-                dp[i][curSum]=0
-                return dp[i][curSum]
-            
-            if(dp[i][curSum]!=-1): return dp[i][curSum]
-            
-            if(curSum+coins[i]<=Sum):
-                dp[i][curSum]=solve(i,curSum+coins[i],coins,Sum,dp) + solve(i+1,curSum,coins,Sum,dp)
-                return dp[i][curSum]
-            
-            dp[i][curSum]=solve(i+1,curSum,coins,Sum,dp)
-            return dp[i][curSum]
+        dp=[[0 for i in range(Sum+1)] for j in range(N+1) ]
         
-
-        dp=[[-1 for j in range(Sum+1)] for i in range(N+1)]
-        return solve(0,0,coins,Sum,dp)
-
+        for i in range(N,-1,-1):
+            for j in range(Sum,-1,-1):
+                if(j==Sum): dp[i][j]=1
+                elif i==N: dp[i][j]=0
+                else:
+                    if(j+coins[i]>Sum):
+                        dp[i][j]=dp[i+1][j] if (i+1)<=N else 0
+                    else:
+                        dp[i][j]=(dp[i][j+coins[i]]) + (dp[i+1][j] if (i+1)<=N else 0)
+        
+        return dp[0][0]
 
 
 #{ 
