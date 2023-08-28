0381b834bb6702a3a49d17a2736de448f5319a63
Amar <AmarnathRao36@gmail.com>
2023-08-28 11:41:37 +0530

    28aug

--- test.cpp
+++ test.cpp
@@ -1,48 +1,32 @@
-//{ Driver Code Starts
- 
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function template for C++
-class Solution{
-public:	
-	int isPalindrome(string S)
-	{
-	    // Your code goes here
-        int low=0,high=S.length()-1;
-        while(low<high){
-            if(S[low]!=S[high]){
-                return 0;
-            }
-            low++;
-            high--;
-        }
-        return 1;
-	}
+class MyStack {
+public:
+    
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        
+    }
+    
+    int pop() {
+        
+    }
+    
+    int top() {
+        
+    }
+    
+    bool empty() {
+        
+    }
 };
 
-//{ Driver Code Starts.
-
-int main() 
-{
-   	ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-   
-   	int t;
-   	cin >> t;
-   	while(t--)
-   	{
-   		string s;
-   		cin >> s;
-
-   	    Solution ob;
-
-   		cout << ob.isPalindrome(s) << "\n";
-   	}
-
-    return 0;
-}
-// } Driver Code Ends
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
