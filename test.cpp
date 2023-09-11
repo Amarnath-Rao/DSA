be19f6f8c13b6786e5dde3446da7dd99c2a47372
Amar <AmarnathRao36@gmail.com>
2023-09-11 16:43:06 +0530

    11sept

--- test.cpp
+++ test.cpp
@@ -1,32 +1,24 @@
-class MyStack {
-public:
-    
-    MyStack() {
-        
+#include<bits/stdc++.h>
+using namespace std;
+int fun(int arr[],int n,int f){
+    int k=f;
+    for(int i=0;i<n;i++){
+        if(arr[i]==k){
+            return i;
+        }
     }
-    
-    void push(int x) {
-        
-    }
-    
-    int pop() {
-        
-    }
-    
-    int top() {
-        
-    }
-    
-    bool empty() {
-        
-    }
-};
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
+    return -1;
+}
+int main()
+{
+    // Let us search 3 in below array
+    int arr1[] = { 5, 6, 7, 8, 9, 10, 1, 2, 3 };
+    int n = sizeof(arr1) / sizeof(arr1[0]);
+    int key = 3;
+ 
+    // Function calling
+    cout << "Index of the element is : "
+         << fun(arr1, n, key);
+ 
+    return 0;
+}
