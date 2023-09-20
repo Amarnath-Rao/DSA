7254102dda6535f103188b7a4fea32ad69bec385
Amar <AmarnathRao36@gmail.com>
2023-09-20 17:59:47 +0530

    17sept

---
+++ POTD-Power of 2.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    # Function to check if given number n is a power of two.
+    def isPowerofTwo(self,n):
+        if n==0:
+            return False
+        while n!=1:
+            if (n%2!=0):
+                return False
+            n=n//2
+        return True
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        ob=Solution()
+        if ob.isPowerofTwo(n):
+            print("YES")
+        else:
+            print("NO")
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
