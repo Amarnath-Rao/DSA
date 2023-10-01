209fcde2464dc1aeb7e54a01c3a1b9de0cb7a4bd
Amar <AmarnathRao36@gmail.com>
2023-10-01 11:13:05 +0530

    1oct

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,147 @@
-arr.sort()
-d=None;m=None
-for i in range(n-1):
-    if arr[i]==arr[i+1]:
-        d=arr[i]
-    if arr[i+1]-arr[i]>2:
-        m=arr[i]+1
-if arr[0]!=1:
-    m=1
-elif arr[n-1]!=n:
-    m=n
-print(d,m)
+class pdodudhdbf():
+    def jdhdgcvcbwshquqieoepe(self,hftyhtytyrhwjwiiwscv):
+        if hftyhtytyrhwjwiiwscv==2:return 1
+        if hftyhtytyrhwjwiiwscv==167772161:return 3
+        if hftyhtytyrhwjwiiwscv==469762049:return 3
+        if hftyhtytyrhwjwiiwscv==754974721:return 11
+        if hftyhtytyrhwjwiiwscv==998244353:return 3
+        dodicbanyt=[0]*20
+        dodicbanyt[0]=2
+        shdgfgwiqow=1
+        iddbbffbhd=(hftyhtytyrhwjwiiwscv-1)//2
+        while(iddbbffbhd%2==0):iddbbffbhd//=2
+        dhdh=3
+        while(dhdh*dhdh<=iddbbffbhd):
+            if (iddbbffbhd%dhdh==0):
+                dodicbanyt[shdgfgwiqow]=dhdh
+                shdgfgwiqow+=1
+                while(iddbbffbhd%dhdh==0):
+                    iddbbffbhd//=dhdh
+            dhdh+=2
+        if iddbbffbhd>1:
+            dodicbanyt[shdgfgwiqow]=iddbbffbhd
+            shdgfgwiqow+=1
+        dhdhfhfhfhe=2
+        while(1):
+            jfufuduwuwuw=True
+            for dhdh in range(shdgfgwiqow):
+                if pow(dhdhfhfhfhe,(hftyhtytyrhwjwiiwscv-1)//dodicbanyt[dhdh],hftyhtytyrhwjwiiwscv)==1:
+                    jfufuduwuwuw=False
+                    break
+            if jfufuduwuwuw:
+                return dhdhfhfhfhe
+            dhdhfhfhfhe+=1
+    def bsf(self,iddbbffbhd):
+        jfhfhfgfgeojt=0
+        while(iddbbffbhd%2==0):
+            jfhfhfgfgeojt+=1
+            iddbbffbhd//=2
+        return jfhfhfgfgeojt
+    butterfly_first=True
+    butterfly_inv_first=True
+    sum_e=[0]*30
+    sum_ie=[0]*30
+    def __init__(self,MOD):
+        self.jkbkh=MOD
+        self.dhdhfhfhfhe=self.jdhdgcvcbwshquqieoepe(self.jkbkh)
+
+    def uguguguhuhuh(self,pqefhcbbh,jfhhfgfh):
+        jdhddydyd=len(pqefhcbbh);hftyhtytyrhwjwiiwscv=len(jfhhfgfh)
+        if not(pqefhcbbh) or not(jfhhfgfh):
+            return []
+        if min(jdhddydyd,hftyhtytyrhwjwiiwscv)<=40:
+            if jdhddydyd<hftyhtytyrhwjwiiwscv:
+                jdhddydyd,hftyhtytyrhwjwiiwscv=hftyhtytyrhwjwiiwscv,jdhddydyd
+                pqefhcbbh,jfhhfgfh=jfhhfgfh,pqefhcbbh
+            jfhfhfgfgeojt=[0]*(jdhddydyd+hftyhtytyrhwjwiiwscv-1)
+            for dhdh in range(jdhddydyd):
+                for j in range(hftyhtytyrhwjwiiwscv):
+                    jfhfhfgfgeojt[dhdh+j]+=pqefhcbbh[dhdh]*jfhhfgfh[j]
+                    jfhfhfgfgeojt[dhdh+j]%=self.jkbkh
+            return jfhfhfgfgeojt
+        fjfhedhehe=1<<((jdhddydyd+hftyhtytyrhwjwiiwscv-2).bit_length())
+        pqefhcbbh=pqefhcbbh+[0]*(fjfhedhehe-jdhddydyd)
+        jfhhfgfh=jfhhfgfh+[0]*(fjfhedhehe-hftyhtytyrhwjwiiwscv)
+        self.butterfly(pqefhcbbh)
+        self.butterfly(jfhhfgfh)
+        djhfhfhfhfhfkwi=[0]*fjfhedhehe
+        for dhdh in range(fjfhedhehe):
+            djhfhfhfhfhfkwi[dhdh]=(pqefhcbbh[dhdh]*jfhhfgfh[dhdh])%self.jkbkh
+        self.butterfly_inv(djhfhfhfhfhfkwi)
+        iz=pow(fjfhedhehe,self.jkbkh-2,self.jkbkh)
+        for dhdh in range(jdhddydyd+hftyhtytyrhwjwiiwscv-1):
+            djhfhfhfhfhfkwi[dhdh]=(djhfhfhfhfhfkwi[dhdh]*iz)%self.jkbkh
+        return djhfhfhfhfhfkwi[:jdhddydyd+hftyhtytyrhwjwiiwscv-1]
+class ugghguggghghwoow():
+    jdhddydyd=1
+    cvcgwsgwg=1
+    psodjdbbdbd=2
+    ppppwwwwppp=[0]
+    rtrttrtrtrgrg=None
+    hfhfhf=10**15
+    def __init__(self,V,OP,E):
+        self.jdhddydyd=len(V)
+        self.rtrttrtrtrgrg=OP
+        self.hfhfhf=E
+        self.psodjdbbdbd=(self.jdhddydyd-1).bit_length()
+        self.cvcgwsgwg=1<<self.psodjdbbdbd
+        self.ppppwwwwppp=[E for dhdh in range(2*self.cvcgwsgwg)]
+        for dhdh in range(self.jdhddydyd):
+            self.ppppwwwwppp[self.cvcgwsgwg+dhdh]=V[dhdh]
+        for dhdh in range(self.cvcgwsgwg-1,0,-1):
+            self.update(dhdh)
+    def hhfhdhwhwudjcb(self,p,iddbbffbhd):
+        assert 0<=p and p<self.jdhddydyd
+        p+=self.cvcgwsgwg
+        self.ppppwwwwppp[p]=iddbbffbhd
+        for dhdh in range(1,self.psodjdbbdbd+1):
+            self.update(p>>dhdh)
+    def pehdgsioo(self,p):
+        assert 0<=p and p<self.jdhddydyd
+        return self.ppppwwwwppp[p+self.cvcgwsgwg]
+    def hdhyfgfv(self,kidld,jffhfhfb):
+        assert 0<=kidld and kidld<=jffhfhfb and jffhfhfb<=self.jdhddydyd
+        sml=self.hfhfhf
+        smr=self.hfhfhf
+        kidld+=self.cvcgwsgwg
+        jffhfhfb+=self.cvcgwsgwg
+        while(kidld<jffhfhfb):
+            if (kidld&1):
+                sml=self.rtrttrtrtrgrg(sml,self.ppppwwwwppp[kidld])
+                kidld+=1
+            if (jffhfhfb&1):
+                smr=self.rtrttrtrtrgrg(self.ppppwwwwppp[jffhfhfb-1],smr)
+                jffhfhfb-=1
+            kidld>>=1
+            jffhfhfb>>=1
+        return self.rtrttrtrtrgrg(sml,smr)
+    def hfhfhfhfhfhp(self):
+        return self.ppppwwwwppp[1]
+    def update(self,jfjfjf):
+        self.ppppwwwwppp[jfjfjf]=self.rtrttrtrtrgrg(self.ppppwwwwppp[2*jfjfjf],self.ppppwwwwppp[2*jfjfjf+1])
+    def __str__(self):
+        return str([self.pehdgsioo(dhdh) for dhdh in range(self.jdhddydyd)])
+fhhfhf=int(input())
+pfjfh=[int(dhdh) for dhdh in input().split()]
+jkbkh=998244353
+uffywevchd=[0 for dhdh in range(fhhfhf+1)]
+uffywevchd[0]=1
+for dhdh in range(fhhfhf):
+    uffywevchd[dhdh+1]=((dhdh+1)*uffywevchd[dhdh])%jkbkh
+ehgtgh=[0 for dhdh in range(fhhfhf+1)]
+ehgtgh[fhhfhf]=pow(uffywevchd[fhhfhf],jkbkh-2,jkbkh)
+for dhdh in range(fhhfhf-1,-1,-1):
+    ehgtgh[dhdh]=((dhdh+1)*ehgtgh[dhdh+1])%jkbkh
+fhhghgh=pdodudhdbf(jkbkh)
+fffh=ugghguggghghwoow([[1,ppppwwwwppp] for ppppwwwwppp in pfjfh],fhhghgh.uguguguhuhuh,[1])
+peiej=fffh.hfhfhfhfhfhp()
+jfjffh=0
+for jfjfjf in range(fhhfhf):
+    jfjffh+=pow(fhhfhf,jfjfjf-1+jkbkh-1,jkbkh)*uffywevchd[fhhfhf-jfjfjf]*peiej[jfjfjf]
+    jfjffh%=jkbkh
+hbgrfg=jfjffh*pow(fhhfhf,jkbkh-2,jkbkh)
+hbgrfg%=jkbkh
+for dhdh in range(fhhfhf):
+    hbgrfg*=ehgtgh[pfjfh[dhdh]]
+    hbgrfg%=jkbkh
+print(hbgrfg)
--- test.cpp
+++ test.cpp
@@ -1,80 +1,92 @@
-// C++ Program to print the last 10 lines.
-// If number of lines is less than 10,
-// then print all lines.
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define fi first
+#define se second
+#define ll long long
+#define pb push_back
+#define db double
+#define inf 0x3f3f3f3f
 using namespace std;
-#define DELIM '\n'
-
-/* Function to print last n lines of a given string */
-void print_last_lines(char *str, int n)
-{
-	/* Base case */
-	if (n <= 0)
-	return;
-
-	size_t cnt = 0; // To store count of '\n' or DELIM
-	char *target_pos = NULL; // To store the output position in str
-
-	/* Step 1: Find the last occurrence of DELIM or '\n' */
-	target_pos = strrchr(str, DELIM);
-
-	/* Error if '\n' is not present at all */
-	if (target_pos == NULL)
-	{
-		cout << "ERROR: string doesn't contain '\\n' character\n";
-		return;
-	}
-
-	/* Step 2: Find the target position from
-	where we need to print the string */
-	while (cnt < n)
-	{
-		// Step 2.a: Find the next instance of '\n'
-		while (str < target_pos && *target_pos != DELIM)
-			--target_pos;
-
-		/* Step 2.b: skip '\n' and increment count of '\n' */
-		if (*target_pos == DELIM)
-			--target_pos, ++cnt;
-
-		/* str < target_pos means str has
-		less than 10 '\n' characters, so break from loop */
-		else
-			break;
-	}
-
-	/* In while loop, target_pos is decremented 2 times,
-	that's why target_pos + 2 */
-	if (str < target_pos)
-		target_pos += 2;
-
-	// Step 3: Print the string from target_pos
-	cout << target_pos << endl;
+const int N=2e5+5;
+const int mod=998244353;
+int n,d[N];
+ll res,inv[N],fac[N];
+ll fpow(ll x,ll y=mod-2){
+    ll z(1);
+    for(;y;y>>=1){
+        if(y&1)z=z*x%mod;
+        x=x*x%mod;
+    }return z;
 }
-
-// Driver Code
-int main(void)
-{
-	char *str1 ="str1\nstr2\nstr3\nstr4\nstr5\nstr6\nstr7\nstr8\nstr9"
-				"\nstr10\nstr11\nstr12\nstr13\nstr14\nstr15\nstr16\nstr17"
-				"\nstr18\nstr19\nstr20\nstr21\nstr22\nstr23\nstr24\nstr25";
-	char *str2 ="str1\nstr2\nstr3\nstr4\nstr5\nstr6\nstr7";
-	char *str3 ="\n";
-	char *str4 = "";
-
-	print_last_lines(str1, 10);
-	cout << "-----------------\n";
-
-	print_last_lines(str2, 10);
-	cout << "-----------------\n";
-
-	print_last_lines(str3, 10);
-	cout << "-----------------\n";;
-
-	print_last_lines(str4, 10);
-	cout << "-----------------\n";
-
-	return 0;
+void init(int n){
+    fac[0]=1;for(int i=1;i<=n;i++)fac[i]=fac[i-1]*i%mod;
+    inv[n]=fpow(fac[n]);for(int i=n;i>=1;i--)inv[i-1]=inv[i]*i%mod;
 }
-
-// This is code is contributed by rathbhupendra
+void add(ll &x,ll y){
+    x=(x+y)%mod;
+}
+ll omega[N<<1][2];
+void ntt(vector<ll>&a,int f=0){
+    int k=0,len=a.size();while((1<<k)<len)k++;
+    for(int i=0;i<len;i++){
+        int t=0;
+        for(int j=0;j<k;j++){
+            if(i>>j&1)t+=(1<<k-j-1);
+        }if(i<t)swap(a[i],a[t]);
+    }
+    for(int l=2;l<=len;l<<=1){
+        int k=l/2;ll x=omega[l][f];
+        for(int i=0;i!=len;i+=l){
+            ll y=1;
+            for(int j=0;j<k;j++){
+                ll tmp=a[i+j+k]*y%mod;
+                a[i+j+k]=(a[i+j]-tmp)%mod;
+                a[i+j]=(a[i+j]+tmp)%mod;
+                y=y*x%mod;
+            }
+        }
+    }if(f){
+        ll invlen=fpow(len);
+        for(int i=0;i<len;i++)a[i]=a[i]*invlen%mod;
+    }
+}
+struct poly{
+    vector<ll>a;
+    friend poly operator *(poly a,poly b){
+        assert(a.a.size()==b.a.size());
+        ntt(a.a),ntt(b.a);for(int i=0;i<a.a.size();i++)a.a[i]=a.a[i]*b.a[i]%mod;
+        ntt(a.a,1);return a;
+    }
+};
+poly solve(int l,int r){
+    if(l==r){
+        poly t;t.a.resize(2);
+        t.a[0]=1,t.a[1]=d[l];
+        return t;
+    }int mid=l+r>>1;
+    poly a=solve(l,mid),b=solve(mid+1,r);
+    int len=1;while(len<=r-l+1)len<<=1;
+    a.a.resize(len),b.a.resize(len);
+    a=a*b;
+    return a;
+}
+signed main(){
+	ios::sync_with_stdio(false);
+    cin.tie(0),cout.tie(0);
+    cin>>n;for(int i=1;i<=n;i++)cin>>d[i];init(n);
+    int len=1;while(len<=n)len<<=1;
+    omega[len][0]=fpow(3,(mod-1)/len);
+    omega[len][1]=fpow(3,mod-1-(mod-1)/len);
+    for(int i=len/2;i;i>>=1){
+        omega[i][0]=omega[i<<1][0]*omega[i<<1][0]%mod;
+        omega[i][1]=omega[i<<1][1]*omega[i<<1][1]%mod;
+    }
+    poly f=solve(1,n);ll mul=1;
+    res=fac[n-1];
+    for(int i=1;i<n;i++){
+        add(res,f.a[i]*(n-i)%mod*mul%mod*fac[n-i-1]);
+        mul=mul*n%mod;
+    }
+    for(int i=1;i<=n;i++)res=res*inv[d[i]]%mod;
+    res=res*inv[n]%mod*fac[n-1]%mod;
+    cout<<(res+mod)%mod;
+}
