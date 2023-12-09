5ce5dabbd18cdb63412451416596be83f1cd45bd
Amar <AmarnathRao36@gmail.com>
2023-12-09 11:55:33 +0530

    9dec

--- test.py
+++ test.py
@@ -1,25 +1,28 @@
-def max_subarray_sum(arr):
-    max_so_far = arr[0]
-    max_ending_here = arr[0]
-    start = end = 0
-    s = 0
+def generate_password(number, name):
+    try:
+        # Convert number to scientific notation
+        scientific_notation = "{:.1e}".format(float(number))
 
-    for i in range(1, len(arr)):
-        if arr[i] > max_ending_here + arr[i]:
-            max_ending_here = arr[i]
-            s = i
-        else:
-            max_ending_here += arr[i]
+        # Simplify the digits after the decimal point
+        simplified_exponent = sum(int(digit) for digit in str(scientific_notation.split('e')[1]))
 
-        if max_ending_here > max_so_far:
-            max_so_far = max_ending_here
-            start = s
-            end = i
+        # Create S1 by concatenating the first three letters of each digit
+        s1 = ''.join([word[:3] for word in str(scientific_notation) if word.isalpha() or word == 'e'])
 
-    return max_so_far, arr[start:end+1]
+        # Create S2 by concatenating letters at odd positions in the name
+        s2 = ''.join([name[i-1] for i in range(1, len(name)+1, 2)])
 
-# Test the function
-arr = [1,2,3]
-max_sum, max_subarray = max_subarray_sum(arr)
-print("Maximum subarray sum is", max_sum)
-print("The subarray is", max_subarray)
+        # Combine S1 and S2 with '@' symbol
+        password = f"{s1}@{s2}"
+
+        return password
+    except ValueError:
+        # Invalid number
+        return "Invalid input"
+
+# Input
+T = int(input("Enter the number of test cases: "))
+for _ in range(T):
+    number, name = input().split()
+    result = generate_password(number, name)
+    print(result)
