3216bd456e3a5f0857474cece1f39235f9523296
Amar <AmarnathRao36@gmail.com>
2023-12-12 12:55:31 +0530

    12dec

--- test.py
+++ test.py
@@ -1,36 +1,31 @@
-def max_crossing_subarray(arr, low, mid, high):
-    left_sum = float('-inf')
-    sum_temp = 0
-
-    for i in range(mid, low - 1, -1):
-        sum_temp += arr[i]
-        left_sum = max(left_sum, sum_temp)
-
-    right_sum = float('-inf')
-    sum_temp = 0
-
-    for i in range(mid + 1, high + 1):
-        sum_temp += arr[i]
-        right_sum = max(right_sum, sum_temp)
-
-    return left_sum + right_sum
-
-
-def max_subarray(arr, low, high):
-    if low == high:
-        return arr[low]
-
-    mid = (low + high) // 2
-
-    left_sum = max_subarray(arr, low, mid)
-    right_sum = max_subarray(arr, mid + 1, high)
-    cross_sum = max_crossing_subarray(arr, low, mid, high)
-
-    return max(left_sum, right_sum, cross_sum)
-
-
-# Example usage:
-arr = [-2, -3, 4, -1, -2, 1, 5, -3]
-n = len(arr)
-result = max_subarray(arr, 0, n - 1)
-print("Maximum Subarray Sum:", result)
+class Solution{
+public:
+    int maxGold(int n, int m, vector<vector<int>> M)
+    {
+        vector<vector<int>> dp(n, vector<int> (m));
+        
+        for(int i = 0; i < n; i++){
+            dp[i][m - 1] = M[i][m - 1];
+        }
+        
+        for(int j = m - 2; j > -1; j--){
+            for(int i = 0; i < n; i++){
+                dp[i][j] = dp[i][j + 1];
+                
+                if(i > 0)
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j + 1]);
+                if(i < n - 1)
+                    dp[i][j] = max(dp[i][j], dp[i + 1][j + 1]);
+                    
+                dp[i][j] += M[i][j];
+            }
+        }
+        
+        int ans = 0;
+        
+        for(int i = 0; i < n; i++)
+            ans = max(ans, dp[i][0]);
+            
+        return ans;
+    }
+};
