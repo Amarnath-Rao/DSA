d0037c0d63d2fd4e79562cf6921f092b25fc2d29
Amar <AmarnathRao36@gmail.com>
2023-12-23 13:51:22 +0530

    23dec23

--- test.py
+++ test.py
@@ -1,27 +1,24 @@
-class Solution:
-    def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-        return (R:=list(map(sum, grid))) and (C:=list(map(sum, zip(*grid)))) and \
-            [[2*R[i]+2*C[j]-len(R)-len(C) for j in range(len(C))] for i in range(len(R))]
-    
-
-"""
-class Solution {
-  public:
-    int minCandy(int N, vector<int> &ratings) {
-        vector<int> ans(N, 1);
-        
-        for(int i = 1; i < N; i++){
-            if(ratings[i] > ratings[i - 1])
-                ans[i] = max(ans[i], ans[i - 1] + 1);
+class Solution(object):
+    def isPathCrossing(self, path):
+        moves = {
+            "N": (0, 1),
+            "S": (0, -1),
+            "W": (-1, 0),
+            "E": (1, 0)
         }
         
-        for(int i = N - 2; i > -1; i--){
-            if(ratings[i] > ratings[i + 1])
-                ans[i] = max(ans[i], ans[i + 1] + 1);
-        }
-        
-        return accumulate(ans.begin(), ans.end(), 0LL);
-    }
-};
+        visited = {(0, 0)}
+        x = 0
+        y = 0
 
-"""
+        for c in path:
+            dx, dy = moves[c]
+            x += dx
+            y += dy
+            
+            if (x, y) in visited:
+                return True
+
+            visited.add((x, y))
+        
+        return False
