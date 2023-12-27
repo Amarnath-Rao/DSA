5ddc24d7bac9627741784e96be1f5a2dde8adb18
Amar <AmarnathRao36@gmail.com>
2023-12-27 14:51:26 +0530

    27dec23

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
-def canPartition(nums):
-    total = sum(nums)
-    if total % 2 != 0:
-        return False
-    target = total // 2
-    dp = [False] * (target + 1)
-    dp[0] = True
-    for num in nums:
-        for i in range(target, num - 1, -1):
-            dp[i] = dp[i] or dp[i - num]
-    return dp[target]
+def maxSubArrayLen(arr):
+    max_so_far = max_ending_here = 0
+    start = end = 0
+    beg = 0
+    for i in range(len(arr)):
+        max_ending_here = max_ending_here + arr[i]
+        if max_ending_here < 0:
+            max_ending_here = 0
+            beg = i + 1
+        elif max_so_far < max_ending_here:  
+            max_so_far = max_ending_here
+        start = beg
+        end = i
+    return end - start + 1
 
-nums = [1, 5, 11, 5]
-print(canPartition(nums))
+arr = [1,2,3,4,5,-1,-2,-3,-4,-5,6,7,8,9,10]
+print(maxSubArrayLen(arr))
