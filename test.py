33ab058c1c5d513956afd56726d4c8a86890bb5c
Amar <AmarnathRao36@gmail.com>
2023-12-29 17:52:54 +0530

    29dec23

--- test.py
+++ test.py
@@ -15,3 +15,37 @@ def maxSubArrayLen(arr):
 
 arr = [1,2,3,4,5,-1,-2,-3,-4,-5,6,7,8,9,10]
 print(maxSubArrayLen(arr))
+
+
+"""
+class Solution{
+public:
+	
+	int kSubstrConcat (int n, string s, int k)
+	{
+	    if(n % k)
+	        return 0;
+	        
+	    map<vector<int>, int> substr;
+	    
+	    vector<int> f(26, 0);
+	    
+	    for(int i = 0; i < n; i += k){
+	        for(int j = i - k; j < i and j > -1; j++)
+	            --f[s[j] - 'a'];
+	        for(int j = i; j < i + k; j++)
+	            ++f[s[j] - 'a'];
+	        
+	        substr[f]++;
+	        
+	        if(substr.size() > 2)
+	            return 0;
+	    }
+	    
+	    int freq = (*substr.begin()).second;
+	    
+	    return (freq == 1 or freq >= n / k - 1);
+	}
+};
+
+"""
