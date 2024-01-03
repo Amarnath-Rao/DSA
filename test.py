ccd53410bd645a8717bb8019404c29f69dbaa7e5
Amar <AmarnathRao36@gmail.com>
2024-01-03 15:21:52 +0530

    030124

--- test.py
+++ test.py
@@ -1,18 +1,27 @@
-class Solution(object):
-    def numberOfBeams(self, bank):
-        prev_row_count = 0
-        total = 0
+from collections import deque
 
-        for row in bank:
-            cur_row_count = self.calc(row)
-            if cur_row_count == 0:
-                continue
+def min_moves(start, target, N):
+    dx = [2, 2, -2, -2, 1, 1, -1, -1]
+    dy = [1, -1, 1, -1, 2, -2, 2, -2]
 
-            total += cur_row_count * prev_row_count
-            prev_row_count = cur_row_count
+    queue = deque([(start, 0)])
+    visited = set()
 
-        return total
+    while queue:
+        (x, y), d = queue.popleft()
 
-    def calc(self, s):
-        return sum(int(c) for c in s)
-        
+        if (x, y) == target:
+            return d
+
+        for i in range(8):
+            nx, ny = x + dx[i], y + dy[i]
+            if 1 <= nx <= N and 1 <= ny <= N and (nx, ny) not in visited:
+                queue.append(((nx, ny), d + 1))
+                visited.add((nx, ny))
+
+    return float('inf')
+
+start = (1, 1)
+target = (8, 8)
+N = 8
+print(min_moves(start, target, N))
