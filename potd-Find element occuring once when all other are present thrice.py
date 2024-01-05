92ba3a24584e5ff8bcdc006ce9e36f44f79a2ad4
Amar <AmarnathRao36@gmail.com>
2024-01-05 18:56:06 +0530

    050124

--- potd-Find element occuring once when all other are present thrice.py
+++ potd-Find element occuring once when all other are present thrice.py
@@ -2,16 +2,11 @@
 
 class Solution:
     def singleElement(self, arr, n):
-        mp = dict()
-        for i in range(n):
-            if arr[i] in mp.keys():
-                mp[arr[i]] += 1
-            else:
-                mp[arr[i]] = 1
-        for x in mp:
-            if mp[x]==1:
-                return x
-                break
+        from collections import Counter
+        mp = Counter(arr)
+        for i in mp:
+            if mp[i].values==1:
+                return mp[i].keys
 
 
 #{ 
