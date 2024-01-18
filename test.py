237bd4c9a4a28ce16e2ad987059a1eaf2e5385a5
Amar <AmarnathRao36@gmail.com>
2024-01-18 21:29:39 +0530

    180124

--- test.py
+++ test.py
@@ -11,27 +11,3 @@ a=checkstraightline([[1,2],[2,3],[3,4]])
 b=checkstraightline([[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]])
 #true case
 c=checkstraightline([[1,1],[2,2],[3,3],[4,4],[5,5],[6,6]])
-
-"""
-class Solution:
-    def solve(self, ind, n, st, nums):
-        # base case
-        if ind == n:
-            st.add(tuple(nums))
-            return
-
-        for i in range(ind, n):
-            nums[ind], nums[i] = nums[i], nums[ind]
-            self.solve(ind + 1, n, st, nums)
-            nums[ind], nums[i] = nums[i], nums[ind]
-
-    def uniquePerms(self, arr, n):
-        st = set()
-        ans = []
-        # {ind, n, freq, st}
-        self.solve(0, n, st, arr)
-        for it in st:
-            ans.append(list(it))
-        return ans
-
-"""
