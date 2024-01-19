f51d7323db46e21683a551888c21ab17f7f12dc1
Amar <AmarnathRao36@gmail.com>
2024-01-19 18:51:56 +0530

    190124

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
-#code to check straight line
-def checkstraightline(a):
-    if (a[0][0] == a[1][0] == a[2][0]):
-        print("Yes")
-    elif (a[0][1] == a[1][1] == a[2][1]):
-        print("Yes")
-    else:
-        print("No")
-    
-a=checkstraightline([[1,2],[2,3],[3,4]])
-b=checkstraightline([[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]])
-#true case
-c=checkstraightline([[1,1],[2,2],[3,3],[4,4],[5,5],[6,6]])
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        for r in range(1, len(matrix)):
+            for c in range(len(matrix[0])):
+                if c == 0:
+                    matrix[r][c] += min(matrix[r-1][c], matrix[r-1][c+1])
+                elif c == len(matrix[0])-1:
+                    matrix[r][c] += min(matrix[r-1][c], matrix[r-1][c-1])
+                else:
+                    matrix[r][c] += min(matrix[r-1][c], matrix[r-1][c+1], matrix[r-1][c-1])
+        return min(matrix[-1])
