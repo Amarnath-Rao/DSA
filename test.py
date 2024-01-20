db55ab38c63c1452c8e6b144865da242e6402a34
Amar <AmarnathRao36@gmail.com>
2024-01-20 10:28:20 +0530

    200124

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 class Solution:
-    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        for r in range(1, len(matrix)):
-            for c in range(len(matrix[0])):
-                if c == 0:
-                    matrix[r][c] += min(matrix[r-1][c], matrix[r-1][c+1])
-                elif c == len(matrix[0])-1:
-                    matrix[r][c] += min(matrix[r-1][c], matrix[r-1][c-1])
-                else:
-                    matrix[r][c] += min(matrix[r-1][c], matrix[r-1][c+1], matrix[r-1][c-1])
-        return min(matrix[-1])
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        MOD = 10**9 + 7
+        stack = []
+        sumOfMinimums = 0
+
+        for i in range(len(arr) + 1):
+            while stack and (i == len(arr) or arr[stack[-1]] >= arr[i]):
+                mid = stack.pop()
+                leftBoundary = stack[-1] if stack else -1
+                rightBoundary = i
+
+                count = (mid - leftBoundary) * (rightBoundary - mid) % MOD
+
+                sumOfMinimums += (count * arr[mid]) % MOD
+                sumOfMinimums %= MOD
+            stack.append(i)
+
+        return int(sumOfMinimums)
+
