a502634ad8f56f6741ba8a16c7e488b45b96b5cd
Amar <AmarnathRao36@gmail.com>
2024-01-22 17:47:31 +0530

    220124

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
-class Solution(object):
-    def rob(self, nums):
-        rob, norob = 0, 0
-        for num in nums:
-            newRob = norob + num
-            newNoRob = max(norob, rob)
-            rob, norob = newRob, newNoRob
-        return max(rob, norob)
+private:
+        vector<int> temp;
+        vector<vector<int>> res;
+    void helper(Node * root, int sum){
+        if(!root) return ;
+        
+        temp.push_back(root->key);
+        
+        if(sum - root->key == 0) res.push_back(temp);
+        helper(root->left,sum-root->key);
+        helper(root->right,sum-root->key);
+        
+        temp.pop_back();
+        
+    }
+    public:
+    vector<vector<int>> printPaths(Node *root, int sum){
+        helper(root,sum);
+        return res;
+    }
+
