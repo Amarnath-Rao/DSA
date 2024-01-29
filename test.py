eece4a72d5e3f9295c39b5c91424dd687262ce2c
Amar <AmarnathRao36@gmail.com>
2024-01-29 17:18:12 +0530

    290124

--- test.py
+++ test.py
@@ -43,3 +43,36 @@ class Solution:
             for j in range(65):
                 self.dp[0][i][j] = None
                 self.dp[1][i][j] = None
+
+
+"""
+
+int f(int i,int prev,string &str,int n,vector<vector<int>>&dp)
+    {
+        if(i==n)
+        {
+            return 1;
+        }
+        if(dp[i][prev] != -1)
+        {
+            return dp[i][prev];
+        }
+        int sum=0,ways=0;
+        for(int ind=i;ind<n;ind++)
+        {
+            sum+=str[ind]-'0';
+            if(sum >= prev)
+            {
+                ways+=f(ind+1,sum,str,n,dp);
+            }
+        }
+        return dp[i][prev]=ways;
+    }
+    int TotalCount(string str){
+        // Code here
+        int n=str.size();
+        vector<vector<int>> dp(n,vector<int>(900+1,-1));//since N is till 900 and max value of each digit is 9
+        return f(0,0,str,n,dp);//dp[ind][prev] -> no of ways to reach ind with prev sum as prev
+    }
+
+"""
