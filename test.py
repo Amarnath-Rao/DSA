740ef3dd184cad32aca00b381255c7a787b1a698
Amar <AmarnathRao36@gmail.com>
2024-02-02 20:23:18 +0530

    020224

--- test.py
+++ test.py
@@ -1,23 +1,16 @@
-"""
-class Solution
-{
-  public:
-    //Function to check if a string is Pangram or not.
-    bool checkPangram (string s) {
-        
-        int check[26]={0};
-        for(int i=0;i<s.length();i++)
-        {   
-            if(s[i]!=32)
-            check[tolower(s[i])-'a']++;
-        }
-        for(int i=0;i<26;i++)
-            if(check[i]==0)
-                return 0;
-        return 1;
-        // your code here
-    }
+class Solution:
+    def sequentialDigits(self, low, high):
+        a = []
 
-};
+        for i in range(1, 10):
+            num = i
+            next_digit = i + 1
 
-"""
+            while num <= high and next_digit <= 9:
+                num = num * 10 + next_digit
+                if low <= num <= high:
+                    a.append(num)
+                next_digit += 1
+
+        a.sort()
+        return a
