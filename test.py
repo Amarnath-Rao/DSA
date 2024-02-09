f5c941b6dbef772466bc71374544ff6c7a9be2d7
Amar <AmarnathRao36@gmail.com>
2024-02-09 20:12:05 +0530

    090224

--- test.py
+++ test.py
@@ -1,59 +1,21 @@
 """
- void levelordertraversal(Node* root,vector<vector<int>>&ans){
-        if(root==NULL){
-            return ;
-        }
-        queue<Node*>q;
-        q.push(root);
-        while(!q.empty()){
-            int size=q.size();
-            vector<int>ds;
-            for(int i=0;i<size;i++){
-                Node* temp=q.front();
-                q.pop();
-                ds.push_back(temp->data);
-                if(temp->left){
-                    q.push(temp->left);
-                }
-                if(temp->right){
-                    q.push(temp->right);
-                }
-            }
-            ans.push_back(ds);
-        }
-        
-    }
-    void solve(Node* root,vector<int>&store){
-        if(root==NULL){
-            return;
-        }
-        if(root->left==NULL && root->right==NULL){
-            store.push_back(root->data);
-        }
-        if(root->left){
-            solve(root->left,store);
-            
-        }
-        if(root->right){
-            solve(root->right,store);
-            
-        }
-    }
-    bool check(Node *root)
-    {
-       
-        vector<vector<int>>ans;
-        vector<int>store;
-        levelordertraversal(root,ans);
-        solve(root,store);
-        int n=ans.size();
-        int last=ans[n-1].size();
-        if(store.size()!=last){
-            return false;
-        }
-        return true;
-        
-    }
 
+class Solution{
+    public:
+    //Function to check whether all nodes of a tree have the value 
+    //equal to the sum of their child nodes.
+   
+    int isSumProperty(Node *root)
+    {
+     // Add your code here
+     if(!root || (!root->left && !root->right))return 1;
+          int k=0;
+         if(root->left)k=root->left->data;
+         if(root->right)k+=root->right->data;
+         return (k==root->data) and isSumProperty(root->left) and isSumProperty(root->right);
+    
+     
+    }
+};
 
 """
