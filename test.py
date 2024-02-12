261fb199a7f7241bea8302a4e6e0e03eb593453a
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-02-12 18:54:08 +0530

    120224

--- test.py
+++ test.py
@@ -2,31 +2,19 @@
 
 class Solution{
 public:
-    void solve(int n,vector<int> &ans,unordered_map<int,int> &m,vector<int> &dp){
-        if(dp[n]!=-1){
-            ans.push_back(dp[n-1]);
-            return;
-        }
-        solve(n-1,ans,m,dp);
-        //dp[n] = m.find(dp[n-1]-n)!=m.end()?dp[n-1]-n:dp[n-1]+n;
-        if(dp[n-1]-n>0 && m.find(dp[n-1]-n)==m.end()){
-            dp[n] = dp[n-1]-n;
-        }
-        else{
-            dp[n] = dp[n-1]+n;
-        }
-        m[dp[n]]++;
-        ans.push_back(dp[n]);
-    }
-    vector<int> recamanSequence(int n){
+    long long sequence(int n){
         // code here
-        vector<int>ans;
-        unordered_map<int,int>m;
-        vector<int>dp(n,-1);
-        dp[0]=0;
-        m[0]++;
-        solve(n-1,ans,m,dp);
-        return ans;
+        long long mod = 1e9 + 7;
+        long long out = 0, c = 1;
+        for(int i = 1; i <= n; ++i){
+            long long temp = 1;
+            for(int j = 0; j < i ; ++j){
+                temp *= c++;
+                temp %= mod;
+            }
+            out = (out + temp) % mod;
+        }
+        return out;
     }
 };
  
