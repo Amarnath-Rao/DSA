f2d54e137c745d40d05fa4bf0b814e2e7caaaee9
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-02-15 16:32:48 +0530

    150224

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
         return next((s for s in words if all(s[i]==s[-(i+1)] for i in range(len(s)//2))), "")
+    
+
+"""
+
+class Solution {
+public:
+int isPossible(vector<vector<int>> paths){
+        for(auto i: paths){
+            if(accumulate(i.begin(), i.end(), 0) % 2 != 0)
+                return false;
+        }
+        return true;
+}
+ 
+};
+
+"""
