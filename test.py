b885078247697b56c94f758a5a2bfe84f3a229d1
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-02-17 13:58:57 +0530

    170124

--- test.py
+++ test.py
@@ -14,3 +14,47 @@ class Solution:
             if v[i] != 0:
                 cnt += 1
         return cnt
+
+
+
+"""
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    bool isMaxHeap(int arr[], int n)
+    {
+       for(int i = 1; i<n;i++)if(arr[(i-1)/2]<arr[i])return false;
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+       int n;
+       cin >> n;
+       int a[4*n]={0};
+       for(int i =0;i<n;i++){
+           cin >> a[i];
+       }
+       Solution ob;
+       cout<<ob.isMaxHeap(a, n)<<endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+"""
