4fe0963895da1373049343a8f693e78795bc4884
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-02-18 14:02:24 +0530

    180224

--- test.py
+++ test.py
@@ -19,42 +19,19 @@ class Solution:
 
 """
 
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-
-class Solution{
+class Solution
+{
     public:
-    bool isMaxHeap(int arr[], int n)
-    {
-       for(int i = 1; i<n;i++)if(arr[(i-1)/2]<arr[i])return false;
-        return true;
-    }
-};
-
-//{ Driver Code Starts.
-int main() {
-    
-    int t;
-    cin >> t;
-    while(t--)
-    {
-       int n;
-       cin >> n;
-       int a[4*n]={0};
-       for(int i =0;i<n;i++){
-           cin >> a[i];
-       }
-       Solution ob;
-       cout<<ob.isMaxHeap(a, n)<<endl;
         
-    }
-    return 0;
-}
-
-// } Driver Code Ends
-
+        int sumOfLeafNodes(Node *root ){
+             if(!root)
+                return 0;
+            
+            if(!root->left && !root->right)
+                return root->data;
+            
+            return sumOfLeafNodes(root->left) + sumOfLeafNodes(root->right);
+        }
+};
 
 """
