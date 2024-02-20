36cd9463c48130aa7dfdc88af7f69d39807195c7
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-02-20 15:30:26 +0530

    200224

--- test.py
+++ test.py
@@ -18,27 +18,34 @@ class Solution:
 
 
 """
-int minValue(string s, int k){
-        // code here
-        priority_queue<int> pq;
-        vector<int> alpha(26,0);
-        for(int i = 0; i<(int)s.size(); i++){
-            alpha[s[i]-'a']++;
+class Solution
+{
+public:
+    int helper(int ind,string prefix,string &s, unordered_set<string>&dict){
+        if(ind==s.size())return 0;
+        
+        prefix+=s[ind];
+        if(ind==s.size()-1){
+            return dict.find(prefix)!=dict.end();
         }
-        for(auto i: alpha) pq.push(i);
-        while(k){
-            int high = pq.top();
-            high--;
-            k--;
-            pq.pop();
-            pq.push(high);
+        
+        int op1=0;
+        int op2=0;
+        if(dict.find(prefix)!=dict.end()){
+            op1= helper(ind+1,"",s,dict);
         }
-        int ans = 0;
-        while(pq.size()){
-            ans += pq.top()*pq.top();
-            pq.pop();
-        }
-        return ans;
+        op2= helper(ind+1,prefix,s,dict);
+        return op1||op2;
     }
+    int wordBreak(int n, string s, vector<string> &dictionary) {
+        //code here
+        unordered_set<string>dict;
+        for(auto it:dictionary){
+            dict.insert(it);
+        }
+        return helper(0,"",s,dict);
+    }
+};
+
 
 """
