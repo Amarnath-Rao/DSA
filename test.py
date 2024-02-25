1151584344453a10ac05c2734a8bff75b26b1c1c
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-02-25 13:59:34 +0530

    250224

--- test.py
+++ test.py
@@ -36,23 +36,23 @@ class Solution:
 class Solution
 {
     public:
-    int fun(int n,vector<int>&dp)
-    {
-        if(n==0)
-        return 0;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=max(n,max(n/2,fun(n/2,dp))+max(n/3,fun(n/3,dp))+max(n/4,fun(n/4,dp)));
-        
-        
-    }
-        int maxSum(int n)
-        {
-            //code here.
-            vector<int>dp(n+1,-1);
-            return fun(n,dp);
-            
+   long long f(int ind, vector<int> & vals, long long n, vector<vector<int>> & dp){
+        if(ind<0) return 0;
+        if(n==0) return 1;
+        if(dp[ind][n]!=-1) return dp[ind][n];
+        int pick=0, notPick=0;
+        if(vals[ind]<=n){
+            pick=f(ind, vals, n-vals[ind], dp);
         }
+        notPick=f(ind-1, vals, n, dp);
+        return dp[ind][n] = pick+notPick;
+    }
+    long long int count(long long int n)
+    {
+        vector<int> vals={3,5,10};
+        vector<vector<int>> dp(3, vector<int>(n+1, -1));
+        return f(2, vals, n, dp);
+    }
 };
-
 
 """
