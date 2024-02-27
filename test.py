c9fbcc228f7463fbf1b52a0d4fc6d7f8e8f3b364
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-02-27 10:28:28 +0530

    270224

--- test.py
+++ test.py
@@ -6,3 +6,20 @@ class Solution(object):
         if not p or not q:
             return False
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+
+"""
+
+int* game_with_number(int arr[], int n)
+{
+    
+    // Complete the function
+    for(int i=0;i<n-1;i++){
+        arr[i]= (arr[i] | arr[i+1]);
+    }
+    return arr;
+    
+}
+
+"""
