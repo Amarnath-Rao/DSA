ba10a5d03d24cbac189863305dad8b0e2e5b0f49
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-02-29 15:57:32 +0530

    290224

--- potd-Sum of bit differences.py
+++ potd-Sum of bit differences.py
@@ -12,3 +12,16 @@ def sumBitDifferences(self,arr, n):
             bit_diff = sum([1 for x, y in zip(b1, b2) if x != y])            
             total_sum += bit_diff
     return total_sum
+
+def sumBitDifferences(self,arr, n):
+    result = 0
+    for i in range(32):
+        countOne = 0
+        countZeros = 0
+        for j in range(n):
+            if arr[j] & (1 << i):
+                countOne += 1
+            else:
+                countZeros += 1
+        result += countOne * (countZeros) * 2
+    return result
