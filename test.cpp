e8da41f7ce8bcaf7dcd0e7bb4dc2324424ebee77
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-03-07 16:31:45 +0530

    070324

--- test.cpp
+++ test.cpp
@@ -1,22 +1,46 @@
-int maxIndexDiff(int arr[], int n) 
-    { 
-        // Your code here
-        int left[n];
-        left[0] = arr[0];
-        for(int i=1; i<n; i++){
-            left[i] = min(left[i-1] , arr[i]);
-        }
-        int i = n-1;
-        int j = n-1;
-        int ans = INT_MIN;
-        while(i>=0 && j>=0){
-            if( arr[j] >= left[i]){
-                 ans = max(ans, j-i);
-                 i--;
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string longestSubstring(string s, int n) {
+        
+       vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        int max_length=0;
+        int last=0;
+        for(int i=1;i<=n;i++){
+            for(int j=i+1;j<=n;j++){
+                if(s[i-1]==s[j-1] && dp[i-1][j-1]<(j-i)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                    if(max_length<dp[i][j]){
+                        max_length=dp[i][j];
+                        last=i-1;
+                    }
+                }
             }
-            else{
-                j--;
-            }
         }
-        return ans;
+        if(max_length==0)
+        return "-1";
+        return s.substr(last-max_length+1,max_length);
     }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        string S;
+
+        cin >> N;
+        cin >> S;
+
+        Solution ob;
+        cout << ob.longestSubstring(S, N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
--- test.py
+++ test.py
@@ -1,11 +1,6 @@
-def search(text, pattern):
-    n, m = len(text), len(pattern)
-    res = []
-    for i in range(n - m + 1):
-        if text[i:i+m] == pattern:
-            res.append(i + 1)  # 1-based index
-    return res
-
-text = "geeksforgeeks"
-pattern = "geek"
-print(search(text, pattern))
+A = ['scaler', 'academy']
+x=""
+for i in A:
+    n=len(i)
+    x+=str(i)+str(n)+"~"
+print(x)
