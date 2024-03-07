36775de4fce3050e0171c1039dc7b990c82cac19
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-03-08 04:00:05 +0530

    080324

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-A = ['scaler', 'academy']
-x=""
-for i in A:
-    n=len(i)
-    x+=str(i)+str(n)+"~"
+A = 'scaler6~academy7~'
+A = ''.join([char for char in A if not char.isnumeric()])
+x = A.split('~')
+x = [i for i in x if i]  # remove empty strings
 print(x)
+
+
+# A = ['scaler', 'academy']
+# x=""
+# for i in A:
+#     n=len(i)
+#     x+=str(i)+str(n)+"~"
+# print(x)
