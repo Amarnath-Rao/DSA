3967c2680d0f1ee08ac96fed20ac4158d6061562
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-03-31 11:46:26 +0530

    310324

--- test.cpp
+++ test.cpp
@@ -1,11 +1,21 @@
 /*
 class Solution {
   public:
-    int minValue(Node* root) {
-        // Code here
-        if(root==NULL) return INT_MAX;
-        if(root->left==NULL) return root->data;
-        return minValue(root->left);
+    void inOrder(Node* root , int n , int &ans){
+        if(root == NULL){
+            return ;
+        }
+        inOrder(root->left , n , ans);
+        if(root->key <= n){
+            ans = root->key;
+        }
+        inOrder(root->right , n , ans);
+    }
+    int findMaxForN(Node* root, int n) {
+        // code here
+        int ans = -1;
+        inOrder(root , n , ans);
+        return ans;
     }
 };
 */
