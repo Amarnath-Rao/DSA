353d7c1d3ab6a56359e0774d829bac12dfe0c1d4
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-05-08 21:56:20 +0530

    080524

--- test.cpp
+++ test.cpp
@@ -2,23 +2,14 @@
 using namespace std;
 #define lli long long int
 #include <iostream>
-int sentcount(string sentences){
-    int c=0;
-    for(int i=0;i<sentences.size();i++){
-        if(sentences[i]==' '){
-            c++;
-        }
-    }
-    return c+1;
-}
 void fun(){
-    vector<string>sentences({"alice and bob love leetcode", "i think so too", "this is great thanks very much"});
-    vector<int>l;
-    for(auto x:sentences){
-        l.push_back(sentcount(x));
-    }
-    cout<<*max_element(l.begin(), l.end()); // Modify the code to pass the l vector as two separate iterators to the max function
-
+    vector<string> word1{"ab", "c"}, word2{"a", "bc"};
+    string s1,s2;
+    for(int i=0;i<word1.size();i++){
+        s1+=word1[i];
+    }for(int i=0;i<word2.size();i++) s2+=word2[i];
+    sort(s1.begin(),s1.end());
+    sort(s2.begin(),s2.end());
 }
 int main(){
     fun();
