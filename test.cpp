f903b406260f9f3ba9e902675c2ccc2aa6dfff7b
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-05-24 19:20:21 +0530

    240524

--- test.cpp
+++ test.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 #define lli long long int
 #include <iostream>
+
+
+/*
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        // support variable
+        int res = words.size();
+        bool alpha[26] = {};
+        // populating alpha
+        for (char c: allowed) alpha[c - 'a'] = true;
+        // parsing all the words to see if each character is in alpha
+        for (string word: words) {
+            // in case we find an unallowed character, we decrease res and break
+            for (char c: word) if (!alpha[c - 'a']) {
+                res--;
+                break;
+            }
+        }
+        return res;
+    }
+};
+
+*/
+
 bool isBeautifulSubset(vector<int>& subset, int k) {
     for (int i = 0; i < subset.size(); ++i) {
         for (int j = i + 1; j < subset.size(); ++j) {
