7be379a2fb01cbe453a8411cb33e035a5218079e
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-05-25 15:11:06 +0530

    250524

--- test.cpp
+++ test.cpp
@@ -3,60 +3,38 @@ using namespace std;
 #define lli long long int
 #include <iostream>
 
-
-/*
-
-class Solution {
-public:
-    int countConsistentStrings(string allowed, vector<string>& words) {
-        // support variable
-        int res = words.size();
-        bool alpha[26] = {};
-        // populating alpha
-        for (char c: allowed) alpha[c - 'a'] = true;
-        // parsing all the words to see if each character is in alpha
-        for (string word: words) {
-            // in case we find an unallowed character, we decrease res and break
-            for (char c: word) if (!alpha[c - 'a']) {
-                res--;
+void fun(){
+    //Output: "This is a sentence"
+    string s = "is2 sentence4 This1 a3";
+    vector<string> v;
+    string temp = "";
+    for(int i=0;i<s.size();i++){
+        if(s[i] == ' '){
+            v.push_back(temp);
+            temp = "";
+        }
+        else{
+            temp += s[i];
+        }
+    }
+    v.push_back(temp);
+    vector<string> ans(v.size());
+    for(int i=0;i<v.size();i++){
+        int index = 0;
+        for(int j=0;j<v[i].size();j++){
+            if(isdigit(v[i][j])){
+                index = j;
                 break;
             }
         }
-        return res;
+        int num = stoi(v[i].substr(index));
+        ans[num-1] = v[i].substr(0,index);
     }
-};
-
-*/
-
-bool isBeautifulSubset(vector<int>& subset, int k) {
-    for (int i = 0; i < subset.size(); ++i) {
-        for (int j = i + 1; j < subset.size(); ++j) {
-            if (abs(subset[i] - subset[j]) == k) {
-                return false;
-            }
-        }
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
     }
-    return true;
-}
-
-void fun(){
-    //subset of nums
-    vector<int>nums{2,4,6};int k=2;
-    int n = nums.size();
-    int count = 0;
-
-    for (int mask = 1; mask < (1 << n); ++mask) {
-        vector<int> subset;
-        for (int i = 0; i < n; ++i) {
-            if (mask & (1 << i)) {
-                subset.push_back(nums[i]);
-            }
-        }
-        if (isBeautifulSubset(subset, k)) {
-            ++count;
-        }
-    }
-    cout<<count;
+    cout<<endl;
+    
 
     
 }
