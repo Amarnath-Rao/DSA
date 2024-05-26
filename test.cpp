4e53767ed571accdfff6d4a640321c96e7ca3ac3
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-05-26 23:15:23 +0530

    260524

--- test.cpp
+++ test.cpp
@@ -4,40 +4,16 @@ using namespace std;
 #include <iostream>
 
 void fun(){
-    //Output: "This is a sentence"
-    string s = "is2 sentence4 This1 a3";
-    vector<string> v;
-    string temp = "";
-    for(int i=0;i<s.size();i++){
-        if(s[i] == ' '){
-            v.push_back(temp);
-            temp = "";
-        }
-        else{
-            temp += s[i];
-        }
+    //output:"001"
+    string s="010";int cz=0,co=0;
+    for(int i=0; i<s.size(); i++){
+        if(s[i]=='0') cz++;
+        else co++;
     }
-    v.push_back(temp);
-    vector<string> ans(v.size());
-    for(int i=0;i<v.size();i++){
-        int index = 0;
-        for(int j=0;j<v[i].size();j++){
-            if(isdigit(v[i][j])){
-                index = j;
-                break;
-            }
-        }
-        int num = stoi(v[i].substr(index));
-        ans[num-1] = v[i].substr(0,index);
-    }
-    for(int i=0;i<ans.size();i++){
-        cout<<ans[i]<<" ";
-    }
-    cout<<endl;
-    
-
-    
+    string ans = string(co - 1, '1') + string(cz, '0') + '1';
+    cout<<ans<<endl;
 }
+
 int main(){
     fun();
 }
