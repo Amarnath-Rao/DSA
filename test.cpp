c8fa32c395daa7427d9edc1aa3655419dcc9de34
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-05-28 20:14:10 +0530

    280524

--- test.cpp
+++ test.cpp
@@ -4,14 +4,7 @@ using namespace std;
 #include <iostream>
 
 void fun(){
-    //output:"001"
-    string s="010";int cz=0,co=0;
-    for(int i=0; i<s.size(); i++){
-        if(s[i]=='0') cz++;
-        else co++;
-    }
-    string ans = string(co - 1, '1') + string(cz, '0') + '1';
-    cout<<ans<<endl;
+    
 }
 
 int main(){
