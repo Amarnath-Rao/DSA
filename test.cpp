3ff02ee2546aa2653821ffed2cb5b76c6099382a
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-06-11 19:10:11 +0530

    110624

--- test.cpp
+++ test.cpp
@@ -4,6 +4,31 @@ using namespace std;
 
 void fun(){
     
+
+    /*
+    class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> result;
+        for (int i = 0; i < arr2.size(); i++) {
+            for (int j = 0; j < arr1.size(); j++) {   
+                if (arr1[j] == arr2[i]) {
+                    result.push_back(arr1[j]);
+                    arr1[j] = -1;
+                }
+            }
+        }
+        sort(arr1.begin(), arr1.end());
+        for (int i = 0; i < arr1.size(); i++) {
+            if (arr1[i] != -1) {
+                result.push_back(arr1[i]);
+            }
+        }
+        return result;
+    }
+};
+    
+    */
     int a[] = {2,1,2,1,1,2,2,1};
     int n=sizeof(a)/sizeof(a[0]);
     int b[n];
