eb56985d8d62b8666528b8cfb7f5b9d73052d5d1
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-06-13 10:10:29 +0530

    130624

--- test.cpp
+++ test.cpp
@@ -3,7 +3,10 @@ using namespace std;
 #define lli long long int
 
 void fun(){
-    
+    int a[]={7,10,4,3,20,15};
+    int n=6,k=3;
+    sort(a,a+n);
+    cout<<a[k-1];
 
     /*
     class Solution {
@@ -29,20 +32,7 @@ public:
 };
     
     */
-    int a[] = {2,1,2,1,1,2,2,1};
-    int n=sizeof(a)/sizeof(a[0]);
-    int b[n];
-    for(int i=0; i<n; i++){
-        b[i]=a[i];
-    }
-    int c=0;
-    sort(a,a+n);
-    for(int i=0; i<n; i++){
-        if(a[i]!=b[i]){
-            c++;
-        }
-    }
-    cout<<c;
+
 }
 
 int main(){
