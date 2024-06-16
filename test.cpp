1f7d4032ec51327fd272712a49a97e8c768964e7
Amarnath-Rao <AmarnathRao36@gmail.com>
2024-06-16 17:51:39 +0530

    160624

--- test.cpp
+++ test.cpp
@@ -2,3 +2,18 @@
 using namespace std;
 #define lli long long int
 
+class Solution {
+public:
+int minPatches(vector<int>& nums, int n) {
+    int cnt=0,i=0;
+    long long maxNum=0;
+    while (maxNum<n){
+       if (i<nums.size() && nums[i]<=maxNum+1)
+            maxNum+=nums[i++];
+       else{
+            maxNum+=maxNum+1;cnt++;
+       }
+   }
+   return cnt;
+}
+};
