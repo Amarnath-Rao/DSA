c1998d19c8f7010a0b1e6985e6dcac88f34e33ca
Amar <96937608+Amarnath-Rao@users.noreply.github.com>
2024-11-02 22:47:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth distance/kth-distance.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    bool checkDuplicatesWithinK(vector<int>& arr, int k) {
+        map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            if(mp[arr[i]]){
+                if(i-mp[arr[i]]<=k){
+                    return true;
+                }else{
+                    mp[arr[i]]=i;
+                }
+            }else{
+                mp[arr[i]]=i;
+            }
+        }return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        bool res = obj.checkDuplicatesWithinK(arr, k);
+        if (res)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        // cout << res << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
